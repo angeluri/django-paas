fd696c28145a8a42534bcff45854c1fb462df77f
angeluri <autec041@gmail.com>
2026-01-12 22:58:24 -0600

    Flatten project structure - fix module path

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
-
